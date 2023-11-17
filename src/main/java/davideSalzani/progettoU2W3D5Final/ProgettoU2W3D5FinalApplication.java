d2ec776d60c5624deec1c690c6168cf77091c7ad
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-17 10:15:15 +0100

    Initial commit

---
+++ src/main/java/davideSalzani/progettoU2W3D5Final/ProgettoU2W3D5FinalApplication.java
@@ -0,0 +1,13 @@
+package davideSalzani.progettoU2W3D5Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU2W3D5FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU2W3D5FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davideSalzani/progettoU2W3D5Final/ProgettoU2W3D5FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package davideSalzani.progettoU2W3D5Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoU2W3D5FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
